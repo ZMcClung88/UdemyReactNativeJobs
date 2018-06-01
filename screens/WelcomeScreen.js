b42880b818bf98e0141c704d9229946267c499f8
Zac McClung <zac.mcclung370@gmail.com>
2018-05-31 22:03:46 -0600

    initial commit

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class WelcomeSceen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>WelcomeSceen</Text>
+        <Text>WelcomeSceen</Text>
+        <Text>WelcomeSceen</Text>
+        <Text>WelcomeSceen</Text>
+        <Text>WelcomeSceen</Text>
+        <Text>WelcomeSceen</Text>
+      </View>
+    );
+  }
+}
+
+export default WelcomeSceen;
