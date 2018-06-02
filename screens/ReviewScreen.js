fec8a5882f9b1180b1d744e30a0caa5adecf54e7
Zac McClung <zac.mcclung370@gmail.com>
2018-06-01 23:15:03 -0600

    styling the navbar

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
       title: 'Review Jobs',
-      headerRight: <Button title="Settings" onPress={() => navigation.navigate('settings')} />
+      headerRight: (
+        <Button
+          title="Settings"
+          onPress={() => navigation.navigate('settings')}
+          backgroundColor="rgba(0,0,0,0)"
+          color="rgba(0,122,255,1)"
+        />
+      ),
+      style: {
+        marginTop: Platform.OS === 'android' ? 24 : 0
+      }
     };
   };
 
