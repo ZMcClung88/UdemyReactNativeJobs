2a94460455b93acc7b494224b631fe6bd736d912
Zac McClung <zac.mcclung370@gmail.com>
2018-06-02 11:39:32 -0600

    crazy async/await syntax

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -6,4 +6,12 @@ import { FACEBOOK_LOGIN_SUCCESS } from './types';
 // AsyncStorage.setItem('fb_token', token);
 // AsyncStorage.getItem('fb_token');
 
-export const facebookLogin = () => {};
+export const facebookLogin = () => async dispatch => {
+  let token = await AsycnStorage.getItem('fb_token');
+
+  if (token) {
+    // Dispatch an action saying FB login is done
+  } else {
+    // Start up FB login process
+  }
+};
