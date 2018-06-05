342a1212bbf2626f40d3befa494ea91587b8d513
Zac McClung <zac.mcclung370@gmail.com>
2018-06-04 23:45:39 -0600

    saving changes

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
 import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    return (
+      <Card title={job.title} />
+      <View style={styles.detailWrapper}>
+        <Text>{job.company}</Text>
+        <Text>{job.created_at}</Text>
+      </View>
+      <Text>
+        {job.description}
+      </Text>
+    );
+  }
   render() {
     return (
       <View>
-        <Swipe data={this.props.jobs} />
+        <Swipe data={this.props.jobs} renderCard={this.renderCard} />
       </View>
     );
+  }
+}
+
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
   }
 }
 
