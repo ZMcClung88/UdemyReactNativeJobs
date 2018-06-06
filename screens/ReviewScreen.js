3eaba51c192c28f69f4789667545a7229b2b0484
Zac McClung <zac.mcclung370@gmail.com>
2018-06-06 09:27:47 -0600

    saving changes

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Platform, ScrollView } from 'react-native';
+import { View, Text, Platform, ScrollView, Linking } from 'react-native';
 import { Button, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 
@@ -32,6 +32,7 @@ class ReviewScreen extends Component {
               <Text style={styles.italics}>{job.company}</Text>
               <Text style={styles.italics}>{date}</Text>
             </View>
+            <Button title="Apply Now!" backgroundColor="#03A9F4" onPress={() => Linking.openURL(job.url)} />
           </View>
         </Card>
       );
