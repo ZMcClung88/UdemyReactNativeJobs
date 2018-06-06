f8dba77d300f6bcefe8e6e9b8ea51900f4f473d3
Zac McClung <zac.mcclung370@gmail.com>
2018-06-05 23:19:33 -0600

    Saving changes

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,27 +1,66 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import { MapView } from 'expo';
-import { Card, Button } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import Swipe from '../components/Swipe';
+import * as actions from '../actions';
+import Geocode from 'react-geocode';
+
+// Geocode.setApiKey('AIzaSyAXdQzGlk6GVvuFWswOFxzWRwDg0MhvMTI')
 
 class DeckScreen extends Component {
   renderCard(job) {
+    Geocode.fromAddress(job.location).then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        // console.log(lat, lng);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+
+    const initialRegion = {
+      // longitude: lng,
+      // latitude: lat,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.02
+    };
+
     return (
-      <Card title={job.title} />
-      <View style={styles.detailWrapper}>
-        <Text>{job.company}</Text>
-        <Text>{job.created_at}</Text>
-      </View>
-      <Text>
-        {job.description}
-      </Text>
+      <Card title={job.title}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={Platform.OS === 'android' ? true : false}
+            initialRegion={initialRegion}
+          />
+        </View>
+
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          {/* <Text>{job.created_at}</Text> */}
+        </View>
+        <Text>{job.description}</Text>
+      </Card>
     );
   }
+
+  renderNoMoreCards = () => {
+    return <Card title="No more jobs" />;
+  };
+
   render() {
     return (
-      <View>
-        <Swipe data={this.props.jobs} renderCard={this.renderCard} />
+      <View style={{ marginTop: 20 }}>
+        <Swipe
+          data={this.props.jobs}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+          keyProp="id"
+        />
       </View>
     );
   }
@@ -32,10 +71,11 @@ const styles = {
     flexDirection: 'row',
     justifyContent: 'space-around'
   }
-}
+};
 
 function mapStateToProps({ jobs }) {
-  return { jobs: jobs.results };
+  // console.log('jobs', { jobs });
+  return { jobs };
 }
 
-export default connect(mapStateToProps)(DeckScreen);
+export default connect(mapStateToProps, actions)(DeckScreen);
