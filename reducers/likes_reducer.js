54a6a972eb358eb25b55fc9586158a90d53da832
Zac McClung <zac.mcclung370@gmail.com>
2018-06-06 00:22:32 -0600

    rendering list of liked jobs

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
-import { LIKE_JOB } from '../actions/types';
+// import { REHYDRATE } from 'redux-persist/constants';
+import { LIKE_JOB, CLEAR_LIKED_JOBS } from '../actions/types';
 
-export default function(state, action) {
+export default function(state = [], action) {
   switch (action.type) {
+    // case REHYDRATE:
+    //   return action.payload.likedJobs || [];
+    case CLEAR_LIKED_JOBS:
+      return [];
     case LIKE_JOB:
       return _.uniqBy([action.payload, ...state], 'id');
     default:
