08fe08ce7b1af4dc5346faf0802f40ea5d552fa5
Zac McClung <zac.mcclung370@gmail.com>
2018-06-06 22:55:02 -0600

    generating push tokens

--- services/push_notifications.js
+++ services/push_notifications.js
@@ -1,4 +1,7 @@
 import { Permissions, Notifications } from 'expo';
+import axios from 'axios';
+
+const PUSH_ENDPOINT = 'http://rallycoding.herokuapp.com/api/tokens';
 
 export default async () => {
   let previousToken = await AsyncStorage.getItem('pushtoken');
@@ -6,5 +9,13 @@ export default async () => {
     return;
   } else {
     let { status } = await Permissions.askAsync(Permissions.REMOTE_NOTIFICATIONS);
+
+    if (status !== 'granted') {
+      return;
+    }
+
+    let token = await Notifications.getExponentPushTokenAsync();
+    await axios.post(PUSH_ENDPOINT, { token: { token } });
+    AsyncStorage.setItem('pushtoken', token);
   }
 };
